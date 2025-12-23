7fa68929f794a8100aa5a80a9cc37879aba73dc3
SMY7527 <kdo7527@naver.com>
2025-12-23 15:29:37 +0900

    test

---
+++ jpaDay01/src/test/java/com/example/jpaDay01/JpaDay01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaDay01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDay01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
